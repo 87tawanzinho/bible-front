c655c4f77401503ecb6f2db2d429358d51584375
django <thiago_tawan@outlook.com>
2024-02-26 18:00:46 -0300

    ..

--- app/(main)/inicio/cards.tsx
+++ app/(main)/inicio/cards.tsx
@@ -3,24 +3,25 @@ import React from "react";
 function CardsHome() {
   return (
     <div className="flex overflow-x-auto  gap-2 md:gap-4  lg:gap-8  p-0 lg:p-2  text-center  ">
-      <div>
-        <p className="mb-2">Devocional</p>
-        <div className="h-60 md:h-80  lg:h-96 w-28 md:w-40 lg:w-48 bg-[#98a8b8] rounded-xl scale"></div>
-      </div>
+      <OneCard bg="bg-blue-100" />
+      <OneCard bg="bg-orange-100" />
+      <OneCard bg="bg-red-100" />
+      <OneCard bg="bg-green-100" />
+    </div>
+  );
+}
 
-      <div>
-        <p className="mb-2">Devocional</p>
-        <div className="h-60 md:h-80  lg:h-96 w-28 md:w-40 lg:w-48 bg-[#70886e] rounded-xl"></div>
-      </div>
-
-      <div>
-        <p className="mb-2">Devocional</p>
-        <div className="h-60 md:h-80  lg:h-96 w-28 md:w-40 lg:w-48 bg-[#81705c] rounded-xl"></div>
-      </div>
-
-      <div>
-        <p className="mb-2">Devocional</p>
-        <div className="h-60 md:h-80  lg:h-96 w-28 md:w-40 lg:w-48 bg-[#7a5a61] rounded-xl"></div>
+function OneCard({ bg }: { bg: string }) {
+  return (
+    <div>
+      <p className="mb-2">Devocional</p>
+      <div
+        className={`h-60 md:h-80  lg:h-96 w-28 md:w-40 lg:w-48 ${bg} rounded-xl scale`}
+      >
+        <img
+          src="https://w7.pngwing.com/pngs/439/754/png-transparent-bird-cartoon-drawing-cartoon-birdcage-comics-animals-photography.png"
+          alt=""
+        />
       </div>
     </div>
   );
