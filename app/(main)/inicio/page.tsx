78725aae610d9199e8af24cc629dd769ce0b47cd
django <thiago_tawan@outlook.com>
2024-02-29 00:59:12 -0300

    ..

--- app/(main)/inicio/page.tsx
+++ app/(main)/inicio/page.tsx
@@ -8,7 +8,7 @@ import Card_with_citation_square from "../components/card_with_citation_square";
 function page() {
   return (
     <div className="">
-      <h2 className="text-center lg:text-start text-2xl lg:text-4xl pb-12 font-serif text-sky-950">
+      <h2 className="text-center lg:text-start text-2xl lg:text-4xl pb-12 font-serif ">
         Ensinamentos sobre Jesus
       </h2>
       <CardsHome />
