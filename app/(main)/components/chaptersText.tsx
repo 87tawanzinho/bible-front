3488f2479226869590d5b56f033b7a8ac4378149
django <thiago_tawan@outlook.com>
2024-03-01 13:05:40 -0300

    ..

---
+++ app/(main)/components/chaptersText.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function ChaptersText({ cap, total }: { cap: string; total: string }) {
+  return (
+    <div className="flex items-center justify-between gap-2 font-serif px-2 border-b ">
+      <h2>Capitulo {cap}</h2>
+      <h3>Total de lições: {total}</h3>
+    </div>
+  );
+}
+
+export default ChaptersText;
--- app/(main)/inicio/page.tsx
+++ app/(main)/inicio/page.tsx
@@ -4,17 +4,18 @@ import { FaChevronDown, FaSortDown } from "react-icons/fa";
 import Title_and_arrowDown from "../components/title_and_arrowDown";
 import { BsArrowDown } from "react-icons/bs";
 import Card_with_citation_square from "../components/card_with_citation_square";
+import ChaptersText from "../components/chaptersText";
 
 function page() {
   return (
     <div className="">
-      <h2 className="text-center underline lg:text-start text-2xl lg:text-4xl pb-12  ">
-        Ensinamentos sobre Jesus
-      </h2>
-
-      <div className="flex items-center gap-2 font-serif px-2">
-        <h2>Comece por aqui</h2>
+      <div className="text-center  lg:text-start text-2xl lg:text-4xl pb-12  ">
+        <h2 className="underline">Ensinamentos sobre Jesus</h2>
+        <h4 className="text-[12px]">
+          Comece a caminhar com <span className="font-bold">Deus</span>.
+        </h4>
       </div>
+      <ChaptersText cap="1" total="4" />
       <CardsHome />
 
       <Card_with_citation_square />
