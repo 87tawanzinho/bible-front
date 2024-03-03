899b97444e6b7513ccc63a566989d88187465a4c
django <thiago_tawan@outlook.com>
2024-03-02 21:31:36 -0300

    ..

--- app/(main)/loading.tsx
+++ app/(main)/loading.tsx
@@ -3,7 +3,13 @@ import { Player } from "@lottiefiles/react-lottie-player";
 import React from "react";
 import gif from "@/public/bird-loading.json";
 function Loading() {
-  return <div className="loader"></div>;
+  return (
+    <div className="leap-frog mt-4 ml-4">
+      <div className="leap-frog__dot"></div>
+      <div className="leap-frog__dot"></div>
+      <div className="leap-frog__dot"></div>
+    </div>
+  );
 }
 
 export default Loading;
