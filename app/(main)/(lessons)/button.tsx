1bc81b0f67fd937638454e4bacea6fd6daae27a0
django <thiago_tawan@outlook.com>
2024-03-09 15:17:16 -0300

    >.

--- app/(main)/(lessons)/button.tsx
+++ app/(main)/(lessons)/button.tsx
@@ -5,7 +5,8 @@ import { useCardsContext } from "../context/cardsData";
 import caracol from "@/public/caracol.png";
 import Image from "next/image";
 function Button({ n, index }: { n: number; index: number }) {
-  const { AxiosTakeCards, setLoading } = useCardsContext();
+  const { cardsData, setCardsData, AxiosTakeCards, loading, setLoading } =
+    useCardsContext();
   const router = useRouter();
 
   return (
@@ -23,9 +24,16 @@ function Button({ n, index }: { n: number; index: number }) {
         }
       }}
     >
-      <button className="bg-white border-2  flex items-center justify-center gap-2  lg:w-96 text-black rounded-lg  p-2 w-11/12 ">
-        +1 <Image src={caracol} alt="caracol" className="h-8 w-8" />
-      </button>
+      {!loading ? (
+        <button className="bg-white border-2  flex items-center justify-center gap-2  lg:w-96 text-black rounded-lg  p-2 w-11/12 ">
+          +1 <Image src={caracol} alt="caracol" className="h-8 w-8" />
+        </button>
+      ) : (
+        <div className="ld-ripple">
+          <div></div>
+          <div></div>
+        </div>
+      )}
     </button>
   );
 }
