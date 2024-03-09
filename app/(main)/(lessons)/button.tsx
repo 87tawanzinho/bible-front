2d724b8cbfe8184ffa02c7221225d37570c7d425
django <thiago_tawan@outlook.com>
2024-03-09 15:12:54 -0300

    >.

--- app/(main)/(lessons)/button.tsx
+++ app/(main)/(lessons)/button.tsx
@@ -5,33 +5,27 @@ import { useCardsContext } from "../context/cardsData";
 import caracol from "@/public/caracol.png";
 import Image from "next/image";
 function Button({ n, index }: { n: number; index: number }) {
-  const { cardsData, setCardsData, AxiosTakeCards, loading, setLoading } =
-    useCardsContext();
+  const { AxiosTakeCards, setLoading } = useCardsContext();
   const router = useRouter();
 
   return (
     <button
       className=" mt-10 w-full flex justify-center lg:justify-start"
       onClick={() => {
+        setLoading(true);
         try {
-          setLoading(true);
           axiosToggleCompleted("firstChapter", n);
           AxiosTakeCards();
         } catch (error) {
           console.log(error);
+        } finally {
+          setLoading(false);
         }
       }}
     >
-      {!loading ? (
-        <button className="bg-white border-2  flex items-center justify-center gap-2  lg:w-96 text-black rounded-lg  p-2 w-11/12 ">
-          +1 <Image src={caracol} alt="caracol" className="h-8 w-8" />
-        </button>
-      ) : (
-        <div className="ld-ripple">
-          <div></div>
-          <div></div>
-        </div>
-      )}
+      <button className="bg-white border-2  flex items-center justify-center gap-2  lg:w-96 text-black rounded-lg  p-2 w-11/12 ">
+        +1 <Image src={caracol} alt="caracol" className="h-8 w-8" />
+      </button>
     </button>
   );
 }
